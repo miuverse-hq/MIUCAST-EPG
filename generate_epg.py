25c94d0f654eded9cb72f13c41ef2c0cc4b2c3a0
miuverse-hq <miuverse.hq@gmail.com>
2026-08-15 18:35:17 +0700

    Add EPG generation script in generate_epg.py

---
+++ generate_epg.py
@@ -0,0 +1,50 @@
+import datetime
+
+def generate_epg(days_count=30, output_path="epg.xml"):
+    channels = [
+        {"id": "NBT-HD.th", "name": "NBT HD"},
+        {"id": "NATION-TV.th", "name": "NATION TV"},
+        {"id": "TOP-NEWS.th", "name": "TOP NEWS"}
+    ]
+    
+    start_date = datetime.date.today()
+    
+    xml_lines = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<tv generator-info-name="MIUCAST-EPG-AUTO">\n'
+    ]
+    
+    # สร้างข้อมูล Channel
+    for ch in channels:
+        xml_lines.append(f'\t<channel id="{ch["id"]}">')
+        xml_lines.append(f'\t\t<display-name lang="th">{ch["name"]}</display-name>')
+        xml_lines.append('\t</channel>')
+    xml_lines.append('')
+    
+    # สร้างข้อมูลโปรแกรมล่วงหน้า 30 วัน
+    for i in range(days_count):
+        current_day = start_date + datetime.timedelta(days=i)
+        next_day = current_day + datetime.timedelta(days=1)
+        
+        day_str = current_day.strftime("%Y%m%d")
+        next_day_str = next_day.strftime("%Y%m%d")
+        
+        for ch in channels:
+            # ช่วงเวลา 12:00 ถึง 00:00 (ของวันถัดไป)
+            xml_lines.append(f'\t<programme start="{day_str}120000 +0000" stop="{next_day_str}000000 +0000" channel="{ch["id"]}">')
+            xml_lines.append('\t\t<title lang="th">ไม่มีข้อมูลรายการโทรทัศน์อิเล็กทรอนิกส์ (EPG)</title>')
+            xml_lines.append('\t</programme>')
+            
+            # ช่วงเวลา 00:00 ถึง 12:00 (ของวันถัดไป)
+            xml_lines.append(f'\t<programme start="{next_day_str}000000 +0000" stop="{next_day_str}120000 +0000" channel="{ch["id"]}">')
+            xml_lines.append('\t\t<title lang="th">ไม่มีข้อมูลรายการโทรทัศน์อิเล็กทรอนิกส์ (EPG)</title>')
+            xml_lines.append('\t</programme>')
+            xml_lines.append('')
+            
+    xml_lines.append('</tv>')
+    
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write("\n".join(xml_lines))
+
+if __name__ == "__main__":
+    generate_epg(30, "epg.xml")
